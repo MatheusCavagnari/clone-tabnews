80e88b9319efb3c7486ada4bf8b34541ce45ac5a
Matheus Cavagnari <53799436+MatheusCavagnari@users.noreply.github.com>
2023-05-05 01:34:14 +0000

    modificação da frase

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home() {
-  return <h1>Amor eu te amo tanto!! Quero ficar com vc para sempre </h1>;
+  return <h1>Amor eu te amo tanto!! Quero ficar com vc para sempre.... </h1>;
 }
 
 export default Home;
