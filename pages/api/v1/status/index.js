ca7d0a39f2039328b6deddaa696c72a4cd3ece15
Matheus Cavagnari <53799436+MatheusCavagnari@users.noreply.github.com>
2024-01-18 02:24:29 +0000

    feat: criação do retorno de

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,11 +1,31 @@
 import database from "infra/database.js";
 
 async function status(request, response) {
-  const result = await database.query("SELECT 1 + 1 as sum;");
-  console.log(result.rows);
-  response
-    .status(200)
-    .json({ chave: "alunos do curso.dev são pessoas acima da média" });
+  const activeConnections = await database.query(
+    `SELECT sum(numbackends) FROM pg_stat_database;`
+  );
+
+  const maxConnections = await database.query(
+    `SELECT * FROM pg_settings WHERE name = 'max_connections';`
+  );
+
+  const version = await database.query(
+    ` SELECT split_part(version(), ' ', 2) AS version;`
+  );
+
+  console.log(version.rows[0].version);
+
+  const updateAt = new Date().toISOString();
+  response.status(200).json({
+    update_at: updateAt,
+    dependencies: {
+      database: {
+        active_connections: Number(activeConnections.rows[0].sum),
+        max_connections: maxConnections.rows[0].setting,
+        version: version.rows[0].version,
+      },
+    },
+  });
 }
 
 export default status;
--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -1,4 +1,21 @@
 test("GET to /api/v1/status should return 200", async () => {
   const response = await fetch("http://localhost:3000/api/v1/status");
   expect(response.status).toBe(200);
+
+  const responseBody = await response.json();
+  expect(responseBody.update_at).toBeDefined();
+
+  const parsedUpdateAt = new Date(responseBody.update_at).toISOString();
+  expect(responseBody.update_at).toEqual(parsedUpdateAt);
+
+  const dependencies = responseBody.dependencies;
+  const database = dependencies.database;
+  console.log(database.max_connections);
+
+  expect(database.active_connections).toBeDefined();
+
+  expect(database.max_connections).toBeDefined();
+  expect(database.max_connections).toEqual("100");
+
+  expect(database.version).toBeDefined();
 });
