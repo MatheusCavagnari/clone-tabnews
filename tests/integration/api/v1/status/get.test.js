379e17c170a7724c6879d34efb08cca72fac3f04
Matheus Cavagnari <53799436+MatheusCavagnari@users.noreply.github.com>
2024-12-12 23:48:41 +0000

    test: rename all descriptions to use new pattern

--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -4,25 +4,29 @@ beforeAll(async () => {
   await orchestrator.waitForAllServices();
 });
 
-test("GET to /api/v1/status should return 200", async () => {
-  const response = await fetch("http://localhost:3000/api/v1/status");
-  expect(response.status).toBe(200);
+describe("GET  /api/v1/status", () => {
+  describe("Anonymous user", () => {
+    test("Retriving current system status", async () => {
+      const response = await fetch("http://localhost:3000/api/v1/status");
+      expect(response.status).toBe(200);
 
-  const responseBody = await response.json();
-  expect(responseBody.update_at).toBeDefined();
+      const responseBody = await response.json();
+      expect(responseBody.update_at).toBeDefined();
 
-  const parsedUpdateAt = new Date(responseBody.update_at).toISOString();
-  expect(responseBody.update_at).toEqual(parsedUpdateAt);
+      const parsedUpdateAt = new Date(responseBody.update_at).toISOString();
+      expect(responseBody.update_at).toEqual(parsedUpdateAt);
 
-  const dependencies = responseBody.dependencies;
-  const database = dependencies.database;
+      const dependencies = responseBody.dependencies;
+      const database = dependencies.database;
 
-  expect(database.opened_connections).toBeDefined();
-  expect(database.opened_connections).toEqual(1);
+      expect(database.opened_connections).toBeDefined();
+      expect(database.opened_connections).toEqual(1);
 
-  expect(database.max_connections).toBeDefined();
-  expect(database.max_connections).toEqual(100);
+      expect(database.max_connections).toBeDefined();
+      expect(database.max_connections).toEqual(100);
 
-  expect(database.version).toBeDefined();
-  expect(database.version).toEqual("16.1");
+      expect(database.version).toBeDefined();
+      expect(database.version).toEqual("16.1");
+    });
+  });
 });
